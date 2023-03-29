0e2d6c0eb22356b095d29d77c41b1d9cf2fc9f99
ParkSeoHai <anhhai282003@gmail.com>
2023-03-30 00:44:37 +0700

    base btl

---
+++ DTO_BHQA/DBConnect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_BHQA
+{
+    public class DBConnect
+    {
+        // Connect của Hải
+
+        // Connect của Duy
+
+        // Connect của Duy
+    }
+}
